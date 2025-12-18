3c86ffcd0a99d86d5ffd2445f043919ae7d644d4
bala-amypo <bala@amypo.in>
2025-12-18 14:56:01 +0000

    User commit

--- src/main/java/com/example/demo/model/QueuePosition.java
+++ src/main/java/com/example/demo/model/QueuePosition.java
@@ -13,5 +13,39 @@ public class QueuePosition {
     private Integer position;
     private String status;
 
-    // getters & setters
+    // ===== GETTERS =====
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getTokenId() {
+        return tokenId;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    // ===== SETTERS =====
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTokenId(Long tokenId) {
+        this.tokenId = tokenId;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
