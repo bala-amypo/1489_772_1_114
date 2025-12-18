2677e542a89061342cd12375272c23b8a1ad9b6d
bala-amypo <bala@amypo.in>
2025-12-18 14:39:53 +0000

    User commit

--- src/main/java/com/example/demo/model/Token.java
+++ src/main/java/com/example/demo/model/Token.java
@@ -18,8 +18,54 @@ public class Token {
 
     @ManyToOne
     @JoinColumn(name = "counter_id")
-    @JsonIgnoreProperties("tokens") // prevents infinite loop
+    @JsonIgnoreProperties("tokens")
     private ServiceCounter serviceCounter;
 
-    // getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTokenNumber() {
+        return tokenNumber;
+    }
+
+    public void setTokenNumber(String tokenNumber) {
+        this.tokenNumber = tokenNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getIssuedAt() {
+        return issuedAt;
+    }
+
+    public void setIssuedAt(LocalDateTime issuedAt) {
+        this.issuedAt = issuedAt;
+    }
+
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    public ServiceCounter getServiceCounter() {
+        return serviceCounter;
+    }
+
+    public void setServiceCounter(ServiceCounter serviceCounter) {
+        this.serviceCounter = serviceCounter;
+    }
 }
