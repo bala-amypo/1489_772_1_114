cb8aef5027b181062d6f0f81a4769028047e5c39
bala-amypo <bala@amypo.in>
2025-12-19 14:50:58 +0000

    User commit

--- src/main/java/com/example/demo/repository/QueuePositionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.QueuePosition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QueuePositionRepository extends JpaRepository<QueuePosition, Long> {
-}
--- src/main/java/com/example/demo/repository/TokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.Token;
-
-public interface TokenRepository extends JpaRepository<Token, Long> {
-}
