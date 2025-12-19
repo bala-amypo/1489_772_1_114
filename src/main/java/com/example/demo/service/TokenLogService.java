add6ef1cba9975be7d920e41e14e9ca8a33f7b7a
bala-amypo <bala@amypo.in>
2025-12-19 09:41:33 +0000

    User commit

--- src/main/java/com/example/demo/service/TokenLogService.java
+++ src/main/java/com/example/demo/service/TokenLogService.java
@@ -1,43 +1,15 @@
 package com.example.demo.service;
 
-import com.example.demo.model.TokenLog;
-import com.example.demo.repository.TokenLogRepository;
-import org.springframework.stereotype.Service;
-
+import com.example.demo.entity.TokenLog;
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class TokenLogService {
+public interface TokenLogService {
 
-    private final TokenLogRepository tokenLogRepository;
+    TokenLog createTokenLog(TokenLog tokenLog);
 
-    public TokenLogService(TokenLogRepository tokenLogRepository) {
-        this.tokenLogRepository = tokenLogRepository;
-    }
+    List<TokenLog> getAllTokenLogs();
 
-    // Create
-    public TokenLog createLog(TokenLog tokenLog) {
-        return tokenLogRepository.save(tokenLog);
-    }
+    TokenLog getTokenLogById(Long id);
 
-    // Read All
-    public List<TokenLog> getAllLogs() {
-        return tokenLogRepository.findAll();
-    }
-
-    // Read by Id
-    public Optional<TokenLog> getLogById(Long id) {
-        return tokenLogRepository.findById(id);
-    }
-
-    // Update
-    public TokenLog updateLog(TokenLog tokenLog) {
-        return tokenLogRepository.save(tokenLog);
-    }
-
-    // Delete
-    public void deleteLog(Long id) {
-        tokenLogRepository.deleteById(id);
-    }
+    List<TokenLog> getTokenLogsByTokenId(Long tokenId);
 }
