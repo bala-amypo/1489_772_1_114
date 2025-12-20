97aff2b436e7a53042717b209714c829a15fff5b
bala-amypo <bala@amypo.in>
2025-12-20 19:05:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/QueuePositionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/QueuePositionServiceImpl.java
@@ -1,3 +1,12 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.entity.QueuePosition;
+import com.example.demo.repository.QueuePositionRepository;
+import com.example.demo.service.QueuePositionService;
+
 @Service
 public class QueuePositionServiceImpl implements QueuePositionService {
 
