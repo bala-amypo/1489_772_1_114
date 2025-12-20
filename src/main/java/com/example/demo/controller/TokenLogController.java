697b3fed112a3da01b32fc66f5ef9d8ed15b72bd
bala-amypo <bala@amypo.in>
2025-12-20 14:31:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/TokenLogController.java
+++ src/main/java/com/example/demo/controller/TokenLogController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.TokenLog;
 import com.example.demo.service.TokenLogService;
+
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,13 +16,13 @@ public class TokenLogController {
     @Autowired
     private TokenLogService tokenLogService;
 
-    // POST – Create log
+    // CREATE LOG
     @PostMapping
-    public TokenLog createLog(@RequestBody TokenLog tokenLog) {
+    public TokenLog createLog(@Valid @RequestBody TokenLog tokenLog) {
         return tokenLogService.saveLog(tokenLog);
     }
 
-    // GET – Logs by Token ID
+    // GET LOGS BY TOKEN ID
     @GetMapping("/token/{tokenId}")
     public List<TokenLog> getLogsByToken(@PathVariable Long tokenId) {
         return tokenLogService.getLogsByTokenId(tokenId);
--- src/main/java/com/example/demo/service/impl/TokenLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TokenLogServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.entity.Token;
 import com.example.demo.entity.TokenLog;
 import com.example.demo.repository.TokenLogRepository;
+import com.example.demo.repository.TokenRepository;
 import com.example.demo.service.TokenLogService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +17,18 @@ public class TokenLogServiceImpl implements TokenLogService {
     @Autowired
     private TokenLogRepository tokenLogRepository;
 
+    @Autowired
+    private TokenRepository tokenRepository;
+
     @Override
     public TokenLog saveLog(TokenLog tokenLog) {
+
+        Token token = tokenRepository.findById(tokenLog.getTokenId())
+                .orElseThrow(() ->
+                        new RuntimeException("Token not found with id: " + tokenLog.getTokenId())
+                );
+
+        tokenLog.setToken(token);
         return tokenLogRepository.save(tokenLog);
     }
 
