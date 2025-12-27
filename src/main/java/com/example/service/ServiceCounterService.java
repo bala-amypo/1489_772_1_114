42d6f346068b5b5415f961e81b01aa3352caa281
bala-amypo <bala@amypo.in>
2025-12-27 19:23:40 +0000

    User commit

--- src/main/java/com/example/service/ServiceCounterService.java
+++ src/main/java/com/example/service/ServiceCounterService.java
@@ -1,8 +1,9 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.QueuePosition;
+import com.example.demo.entity.ServiceCounter;
+import java.util.List;
 
-public interface QueueService {
-    QueuePosition updateQueuePosition(Long tokenId, Integer position);
-    QueuePosition getPosition(Long tokenId);
+public interface ServiceCounterService {
+    ServiceCounter addCounter(ServiceCounter counter);
+    List<ServiceCounter> getActiveCounters();
 }
